056a1b71dcfb6dd37c4dd957506cb8e46f5306f1
Evann.lietard.etu <evann.lietard.etu@univ-lille.fr>
2024-03-31 15:41:13 +0200

    Do the same thing for Combiner5

---
+++ guava/src/com/google/common/util/concurrent/ClosingFunction5.java
@@ -0,0 +1,44 @@
+package com.google.common.util.concurrent;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.concurrent.Executor;
+
+/**
+ * A function that returns a value when applied to the values of the five futures passed to
+ * {@link #whenAllSucceed(ClosingFuture, ClosingFuture, ClosingFuture, ClosingFuture,
+ * ClosingFuture)}.
+ *
+ * @param <V1> the type returned by the first future
+ * @param <V2> the type returned by the second future
+ * @param <V3> the type returned by the third future
+ * @param <V4> the type returned by the fourth future
+ * @param <V5> the type returned by the fifth future
+ * @param <U> the type returned by the function
+ */
+@FunctionalInterface
+public interface ClosingFunction5<
+        V1 extends @Nullable Object,
+        V2 extends @Nullable Object,
+        V3 extends @Nullable Object,
+        V4 extends @Nullable Object,
+        V5 extends @Nullable Object,
+        U extends @Nullable Object> {
+    /**
+     * Applies this function to five inputs, or throws an exception if unable to do so.
+     *
+     * <p>Any objects that are passed to {@link DeferredCloser#eventuallyClose(Object, Executor)
+     * closer.eventuallyClose()} will be closed when the {@link ClosingFuture} pipeline is done
+     * (but not before this method completes), even if this method throws or the pipeline is
+     * cancelled.
+     */
+    @ParametricNullness
+    U apply(
+            DeferredCloser closer,
+            @ParametricNullness V1 value1,
+            @ParametricNullness V2 value2,
+            @ParametricNullness V3 value3,
+            @ParametricNullness V4 value4,
+            @ParametricNullness V5 value5)
+            throws Exception;
+}
